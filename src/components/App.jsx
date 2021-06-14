1e59c8ccca58fdc5e82dd8d5c9d5bc954c7c6080
jaykakkar31 <jaykakkar31@gmail.com>
2021-06-15 00:05:53 +0530

    first commit

---
+++ src/components/App.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+import Header from "./header"
+function App() {
+  return (
+    <div className="homepage">
+      <Header className="App-header"/>
+    </div>
+  );
+}
+
+export default App;
